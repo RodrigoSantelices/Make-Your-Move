33c2170a447dc70eb19f13ca5d24350e3319cf28
Rodrigo Santelices <rcsantelices@gmail.com>
2018-05-09 19:45:34 -0400

    travis build

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+'use strict'
+
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080, ()=>{
-  console.log('server is running')
+
+if (require.main === module){
+app.listen(process.env.PORT || 8080, function(){
+  console.info(`App is listening on ${this.address().port}`);
 });
+}
+
+module.exports = app;
