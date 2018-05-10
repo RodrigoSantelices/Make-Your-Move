6826ff14d6b3f8b0dc2e65939f64d1ed74d97313
Rodrigo Santelices <rcsantelices@gmail.com>
2018-05-09 20:05:54 -0400

    lets start

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
 
 if (require.main === module){
 app.listen(process.env.PORT || 8080, function(){
