f0a9a6fcec21821c9cf834a31d1a9d607f5a39f4
Rodrigo Santelices <rcsantelices@gmail.com>
2018-05-09 22:28:23 -0400

    changes

--- server.js
+++ server.js
@@ -12,5 +12,34 @@ app.listen(process.env.PORT || 8080, function(){
   console.info(`App is listening on ${this.address().port}`);
 });
 }
+const MOCK_MOVE_LIST ={
+  "moveList":[
+      {
+        "id":"1111",
+        "item-name": "foof",
+        "item-location": "car",
+        "item-value":"$50",
+        "item-image":"pic goes here",
+        "status":"Loaded"
+      },
 
+      {
+        "id":"2222",
+        "item-name": "chair",
+        "item-location": "truck",
+        "item-value":"$100",
+        "item-image":"pic goes here",
+        "status":"Unloaded"
+      },
+
+      {
+        "id":"3333",
+        "item-name": "bed",
+        "item-location": "home",
+        "item-value":"$350",
+        "item-image":"pic goes here",
+        "status":"Loaded"
+      },
+  ]
+}
 module.exports = app;
