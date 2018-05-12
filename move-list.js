6b32d9dcb96c57ed03cc83f706952bf579cb8394
Rodrigo Santelices <rcsantelices@gmail.com>
2018-05-12 10:37:48 -0400

    callbackFN

--- move-list.js
+++ move-list.js
@@ -2,7 +2,7 @@ const MOCK_MOVE_LIST ={
   "moveList":[
       {
       //  "id":"1111",
-      //  "name": "foof",
+      "name": "foof",
       //  "location": "car",
       //  "value":"$50",
       //  "image":"pic goes here",
@@ -11,7 +11,7 @@ const MOCK_MOVE_LIST ={
 
       {
       //  "id":"2222",
-      //  "name": "chair",
+       "name": "chair",
       //  "location": "truck",
       //  "value":"$100",
       //  "image":"pic goes here",
@@ -20,7 +20,7 @@ const MOCK_MOVE_LIST ={
 
       {
       //  "id":"3333",
-      //  "name": "bed",
+       "name": "bed",
       //  "location": "home",
       //  "value":"$350",
       //  "image":"pic goes here",
@@ -35,8 +35,8 @@ const MOCK_MOVE_LIST ={
 // timeout function that returns mock data, it will
 // use jQuery's AJAX functionality to make a call
 // to the server and then run the callbackFn
-function getMoveList(callback){
-  setTimeout(function(){callback(MOCK_MOVE_LIST)},1);
+function getMoveList(callbackFN){
+  setTimeout(function(){callbackFN(MOCK_MOVE_LIST)},1);
 }
 
 // this function stays the same when we connect
@@ -45,7 +45,7 @@ function getMoveList(callback){
 function displayMoveList(data){
   for(index in data.moveList){
     $(`.move-list-js`).append(
-      '<p>'+ data.moveList[index].status + '</p>');
+      '<p>'+ data.moveList[index].name + '</p>');
   }
 }
 //this function can stay the same
