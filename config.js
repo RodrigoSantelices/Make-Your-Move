3e77a115a5c7e1133b4274583792563e05a9f461
Rodrigo Santelices <rcsantelices@gmail.com>
2018-05-30 23:24:36 -0400

    some styling done to home page

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/MakeYourMove'//'mongodb://mongodb://rodrigo:TestMe1@ds229290.mlab.com:29290/make-your-move';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/MakeYourMove';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/MakeYourMoveTest';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
